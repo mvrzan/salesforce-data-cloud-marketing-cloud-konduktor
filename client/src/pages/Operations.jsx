a67e7916d83b668c58ab6a016fd63df7a3f14bec
mvrzan <mvrzan@icloud.com>
2024-09-23 17:01:53 -0400

    Add Operations page component

---
+++ client/src/pages/Operations.jsx
@@ -0,0 +1,14 @@
+import Body from "../components/Body/Body";
+import Heading from "../components/Heading/Heading";
+
+const Operations = () => {
+  return (
+    <Body>
+      <Heading as="h3" variant="heading30">
+        Operations
+      </Heading>
+    </Body>
+  );
+};
+
+export default Operations;
