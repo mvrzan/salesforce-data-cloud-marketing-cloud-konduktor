7fd604ad89967f8b61c86283f42cb99558241963
mvrzan <mvrzan@icloud.com>
2024-09-23 17:01:44 -0400

    Refactor routing in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { CustomizationProvider } from "@twilio-paste/core/customization";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <CustomizationProvider>
+        <App />
+      </CustomizationProvider>
+    </BrowserRouter>
   </StrictMode>
 );
