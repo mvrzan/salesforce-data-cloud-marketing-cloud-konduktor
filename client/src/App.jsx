0379b4eaefc5a3d0ed678d5ec51bcb828ff26de2
mvrzan <mvrzan@icloud.com>
2024-09-25 16:05:34 -0400

    add QueryClientProvider for React Query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Welcome from "./pages/Welcome";
 import Operations from "./pages/Operations";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/operations" element={<Operations />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/operations" element={<Operations />} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
