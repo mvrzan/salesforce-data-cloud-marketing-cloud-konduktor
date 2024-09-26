c9173365ed87483f516c61ce4c9a53bfdbd8a01c
mvrzan <mvrzan@icloud.com>
2024-09-26 13:30:02 -0400

    Refactor useBearStore to update segments and emailTemplates state

--- client/src/hooks/useBearStore.js
+++ client/src/hooks/useBearStore.js
@@ -2,7 +2,10 @@ import { create } from "zustand";
 
 const useBearStore = create((set) => ({
   segments: [],
+  emailTemplates: [],
   updateSegments: (incomingSegments) => set(() => ({ segments: [...incomingSegments] })),
+  updateEmailTemplates: (incomingEmailTemplates) =>
+    set((state) => ({ emailTemplates: [...state.emailTemplates, incomingEmailTemplates] })),
 }));
 
 export default useBearStore;
