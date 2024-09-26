cc899cdbb8930375f340f671fc61a248d4ae1b8d
mvrzan <mvrzan@icloud.com>
2024-09-26 11:51:56 -0400

    remove initial segments and update segments state

--- client/src/hooks/useBearStore.js
+++ client/src/hooks/useBearStore.js
@@ -1,10 +1,7 @@
 import { create } from "zustand";
 
 const useBearStore = create((set) => ({
-  segments: [
-    { name: "segment1", segmentId: 1 },
-    { name: "segment2", segmentId: 2 },
-  ],
+  segments: [],
   updateSegments: (incomingSegments) => set(() => ({ segments: [...incomingSegments] })),
 }));
 
