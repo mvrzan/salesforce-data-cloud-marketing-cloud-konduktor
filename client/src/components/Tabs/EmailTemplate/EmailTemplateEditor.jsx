e5ed3a0ce82798146bd5aa375c95af3395c47739
mvrzan <mvrzan@icloud.com>
2024-09-25 22:25:23 -0400

    refactor the component

--- client/src/components/Tabs/EmailTemplate/EmailTemplateEditor.jsx
+++ client/src/components/Tabs/EmailTemplate/EmailTemplateEditor.jsx
@@ -6,10 +6,10 @@ import { Flex } from "@twilio-paste/core/flex";
 import { Text } from "@twilio-paste/core/text";
 import { Stack } from "@twilio-paste/core/stack";
 import { Button } from "@twilio-paste/core/button";
+import { Spinner } from "@twilio-paste/core/spinner";
 
 import StatusModal from "./StatusModal";
 import { sendHtml } from "../../../utils/sendHtml";
-import { sendUiEmail } from "../../../utils/sendUiEmail";
 
 const EmailTemplateEditor = ({ emailName, emailCreatedToast }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,18 +21,24 @@ const EmailTemplateEditor = ({ emailName, emailCreatedToast }) => {
     const unlayer = emailEditorRef.current?.editor;
 
     unlayer?.exportHtml((data) => {
-      const { design, html } = data;
-      console.log("exportHtml", html);
+      const { html } = data;
+      const blob = new Blob([html], { type: "text/html" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `email-template.html`);
+      link.click();
     });
   };
 
   const publishToMarketingCloudHandler = () => {
     setIsModalOpen(true);
-    const unlayer = emailEditorRef.current?.editor;
+    const unlayerEditor = emailEditorRef.current?.editor;
 
-    unlayer?.exportHtml((data) => {
+    unlayerEditor?.exportHtml((data) => {
       setPublishingText("Publishing to Marketing Cloud!");
       const { html } = data;
+
       const fetchHtml = async (html) => {
         const sendHtmlResponse = await sendHtml({ html, emailName });
 
@@ -44,27 +50,8 @@ const EmailTemplateEditor = ({ emailName, emailCreatedToast }) => {
           return;
         }
 
-        setPublishingText("Email created successfully! Creating UI email...");
-
-        const payload = {
-          customerKey: emailName,
-          name: emailName,
-          emailId: sendHtmlResponse.emailId,
-        };
-
-        const sendUiEmailResponse = await sendUiEmail(payload);
-
-        if (!sendUiEmailResponse.ok) {
-          console.error("Failed to create User-Initiated email!", sendUiEmailResponse);
-          setPublishingText("Failed to create User-Initiated email!");
-          setTimeout(() => {
-            setIsModalOpen(false);
-          }, 2000);
-          return;
-        }
-
         setTimeout(() => {
-          setPublishingText("UI email created successfully!");
+          setPublishingText(`Email template ${emailName} created successfully!`);
           setTimeout(() => {
             setIsModalOpen(false);
           }, 2000);
@@ -96,9 +83,21 @@ const EmailTemplateEditor = ({ emailName, emailCreatedToast }) => {
           alignItems="center"
           backgroundColor="rgba(255, 255, 255, 0.8)" // Optional: Add a semi-transparent background
         >
-          <Text fontSize="fontSize50" fontWeight="fontWeightExtrabold" color="colorTextDecorative20">
-            Editor loading...
-          </Text>
+          <Flex hAlignContent="center" vAlignContent="center">
+            <Stack orientation="vertical" spacing="space80">
+              <Box display="flex" alignContent="center" justifyContent="center">
+                <Spinner decorative={false} title="Loading" size="sizeIcon110" color="colorTextDecorative20" />
+              </Box>
+              <Text
+                fontSize="fontSize50"
+                fontWeight="fontWeightExtrabold"
+                color="colorTextLinkStronger"
+                textAlign="center"
+              >
+                Loading Email editor...
+              </Text>
+            </Stack>
+          </Flex>
         </Box>
       )}
 
@@ -114,7 +113,7 @@ const EmailTemplateEditor = ({ emailName, emailCreatedToast }) => {
           <Button variant="primary" onClick={publishToMarketingCloudHandler} disabled={emailName === "" ? true : false}>
             Publish to Marketing Cloud
           </Button>
-          <Button variant="inverse" onClick={testToast}>
+          <Button variant="inverse" onClick={exportHtml}>
             Export HTML
           </Button>
         </Stack>
