c46912842ecadc729f20fc117ae78a1a05783433
mvrzan <mvrzan@icloud.com>
2024-09-27 13:06:45 -0400

    add a new route

--- server/src/routes/route-collection.js
+++ server/src/routes/route-collection.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { getSegment } from "../controllers/get-segment.js";
 import { createEmail } from "../controllers/create-email.js";
+import { getEmailTemplates } from "../controllers/get-email-templates.js";
 import { createUserInitiatedSend } from "../controllers/create-ui-send.js";
 
 const router = Router();
 
 router.get("/segment", getSegment);
 router.post("/create-email", createEmail);
+router.get("/email-templates", getEmailTemplates);
 router.post("/create-ui-email", createUserInitiatedSend);
 
 export default router;
