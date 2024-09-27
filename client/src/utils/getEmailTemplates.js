9837dd43c23ead10d9ead83b976de826e053377e
mvrzan <mvrzan@icloud.com>
2024-09-27 13:07:21 -0400

    Add getEmailTemplates client utility function

---
+++ client/src/utils/getEmailTemplates.js
@@ -0,0 +1,18 @@
+export const getEmailTemplates = async () => {
+  try {
+    const url = "http://localhost:3000/email-templates";
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch segment");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error loading design", error);
+    return [];
+  }
+};
