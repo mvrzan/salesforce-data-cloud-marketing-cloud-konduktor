e017afcb4670e6a2c158b2304a6dd5c0bb96e2b9
mvrzan <mvrzan@icloud.com>
2024-10-02 10:41:31 -0400

    update getSegment controller to getSegments

--- server/src/routes/route-collection.js
+++ server/src/routes/route-collection.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getSegment } from "../controllers/get-segment.js";
+import { getSegments } from "../controllers/get-segments.js";
 import { createEmail } from "../controllers/create-email.js";
 import { getEmailTemplates } from "../controllers/get-email-templates.js";
 import { createUserInitiatedSend } from "../controllers/create-ui-send.js";
 
 const router = Router();
 
-router.get("/segments", getSegment);
+router.get("/segments", getSegments);
 router.post("/create-email", createEmail);
 router.get("/email-templates", getEmailTemplates);
 router.post("/create-ui-email", createUserInitiatedSend);
