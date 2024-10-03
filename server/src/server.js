33e052a69ba9d7bfeea3068dc16c4c68a3f21544
mvrzan <mvrzan@icloud.com>
2024-10-03 16:09:20 -0400

    Initialize database connection on server start

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import router from "./routes/route-collection.js";
+import { initDb } from "./database/initDb.js";
 
 const app = express();
 const corsOptions = {
   origin: "http://localhost:5173",
 };
+
+initDb();
 
 app.use(express.json());
 app.use(cors(corsOptions));
