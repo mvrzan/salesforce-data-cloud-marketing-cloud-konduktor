49a6afcab01d5af88ecec31d08b79a2e5b1ef20d
mvrzan <mvrzan@icloud.com>
2024-10-04 08:03:31 -0400

    update token operation functions

--- server/src/database/token-operations.js
+++ server/src/database/token-operations.js
@@ -1,8 +1,12 @@
-import pool from "./db";
+import { postgresPool } from "./postgres-connection.js";
 
-export const writeSalesforceToken = async (token, expiresAt) => {
+export const writeSalesforceToken = async (token, instance_url, expiresAt) => {
   try {
-    await pool.query("INSERT INTO salesforce_tokens (token, expires_at) VALUES ($1, $2)", [token, expiresAt]);
+    await postgresPool.query("INSERT INTO salesforce_tokens (token, instance_url, expires_at) VALUES ($1, $2 , $3)", [
+      token,
+      instance_url,
+      expiresAt,
+    ]);
     console.log("Salesforce token written to database successfully!");
   } catch (error) {
     console.error("Error writing Salesforce token to database:", error);
@@ -11,7 +15,10 @@ export const writeSalesforceToken = async (token, expiresAt) => {
 
 export const writeMarketingCloudToken = async (token, expiresAt) => {
   try {
-    await pool.query("INSERT INTO marketing_cloud_tokens (token, expires_at) VALUES ($1, $2)", [token, expiresAt]);
+    await postgresPool.query("INSERT INTO marketing_cloud_tokens (token, expires_at) VALUES ($1, $2)", [
+      token,
+      expiresAt,
+    ]);
     console.log("Marketing Cloud token written to database successfully!");
   } catch (error) {
     console.error("Error writing Marketing Cloud token to database:", error);
@@ -20,7 +27,9 @@ export const writeMarketingCloudToken = async (token, expiresAt) => {
 
 export const readSalesforceToken = async () => {
   try {
-    const result = await pool.query("SELECT token, expires_at FROM salesforce_tokens ORDER BY id DESC LIMIT 1");
+    const result = await postgresPool.query(
+      "SELECT token, instance_url, expires_at FROM salesforce_tokens ORDER BY id DESC LIMIT 1"
+    );
     return result.rows[0];
   } catch (error) {
     console.error("Error reading Salesforce token from database:", error);
@@ -29,7 +38,9 @@ export const readSalesforceToken = async () => {
 
 export const readMarketingCloudToken = async () => {
   try {
-    const result = await pool.query("SELECT token, expires_at FROM marketing_cloud_tokens ORDER BY id DESC LIMIT 1");
+    const result = await postgresPool.query(
+      "SELECT token, expires_at FROM marketing_cloud_tokens ORDER BY id DESC LIMIT 1"
+    );
     return result.rows[0];
   } catch (error) {
     console.error("Error reading Marketing Cloud token from database:", error);
