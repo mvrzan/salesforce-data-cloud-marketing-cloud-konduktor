98cef0965b0299724cbf9c705dad5ff2b379fb51
mvrzan <mvrzan@icloud.com>
2024-10-04 10:47:31 -0400

    remove react query references

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Welcome from "./pages/Welcome";
 import Operations from "./pages/Operations";
 
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/operations" element={<Operations />} />
-        </Routes>
-      </QueryClientProvider>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/operations" element={<Operations />} />
+      </Routes>
     </>
   );
 }
